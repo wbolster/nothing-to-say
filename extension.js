d46c42ad22d916b7f6689a168a39a9f92acea4d2
Wouter Bolsterlee <wouter@bolsterl.ee>
2016-09-21 19:09:42 +0200

    only show icon when mic active; many cleanups

--- extension.js
+++ extension.js
@@ -34,12 +34,12 @@ const Microphone = new Lang.Class({
     if (this.stream && this.muted_changed_id) {
       this.stream.disconnect(this.muted_changed_id);
     }
-    this.stream = this.mixer_control.get_default_source();
     let was_active = this.active;
     this.active = false;
+    this.stream = this.mixer_control.get_default_source();
     if (this.stream) {
       this.muted_changed_id = this.stream.connect(
-        'notify::is-muted', Lang.bind(this, this.update));
+        'notify::is-muted', Lang.bind(this, this.notify_muted));
       let recording_apps = this.mixer_control.get_source_outputs();
       for (let i = 0; i < recording_apps.length; i++) {
         let outputStream = recording_apps[i];
@@ -49,11 +49,13 @@ const Microphone = new Lang.Class({
         }
       }
     }
-    if (was_active === null)
-      return;
-    if (this.active == was_active)
-      return;
-    this.emit(this.active ? 'activated' : 'deactivated');
+    this.emit('notify::muted');
+    if (this.active != was_active)
+        this.emit('notify::active');
+  },
+
+  notify_muted: function() {
+    this.emit('notify::muted');
   },
 
   get muted() {
@@ -61,8 +63,15 @@ const Microphone = new Lang.Class({
   },
 
   set muted(muted) {
-    if (this.stream)
-      this.stream.change_is_muted(muted);
+    if (!this.stream)
+      return;
+    this.stream.change_is_muted(muted);
+  },
+
+  get level() {
+    if (!this.stream)
+        return 0;
+    return 100 * this.stream.get_volume() / this.mixer_control.get_vol_max_norm();
   }
 });
 Signals.addSignalMethods(Microphone.prototype);
@@ -70,26 +79,19 @@ Signals.addSignalMethods(Microphone.prototype);
 
 let microphone;
 let button, icon;
+let initialised = false;
 
-function get_stream() {
-  // FIXME: remove in favour of Microphone.stream
-  let control = Main.panel.statusArea.aggregateMenu._volume._control;
-  return control.get_default_source();
-}
-
-function show_osd(text, muted) {
+function show_osd(text, muted, level) {
   let monitor = -1;
   let icon_name = muted ? 'microphone-sensitivity-muted-symbolic' : 'microphone-sensitivity-high-symbolic';
   Main.osdWindowManager.show(
     monitor,
     Gio.Icon.new_for_string(icon_name),
-    text);
+    text,
+    level);
 }
 
 function update_icon(muted) {
-  let stream = get_stream();
-  if (!stream)
-    return;
   let icon_name = muted ? 'microphone-sensitivity-muted-symbolic' : 'microphone-sensitivity-high-symbolic';
   icon.icon_name = icon_name;
 }
@@ -99,48 +101,50 @@ let mute_timeout_id = 0;
 function on_activate(widget, event) {
   if (microphone.muted) {
     microphone.muted = false;
-    update_icon(false);
-    show_osd("Microphone unmuted", false);
+    show_osd("Microphone unmuted", false, microphone.level);
   } else {
     if (mute_timeout_id > 0) {
       Mainloop.source_remove(mute_timeout_id);
-      show_osd("Microphone unmuted", false);
+      show_osd("Microphone unmuted", false, microphone.level);
     }
     mute_timeout_id = Mainloop.timeout_add(
       100,
       function() {
         mute_timeout_id = 0;
         microphone.muted = true;
-        update_icon(true);
-        show_osd("Microphone muted", true);
+        show_osd("Microphone muted", true, 0);
       });
   }
 }
 
 function init() {
+
+  microphone = new Microphone();
   keybindings = (new Settings.Keybindings()).settings;
+  icon = new St.Icon({
+    icon_name: 'microphone-sensitivity-high-symbolic',
+    style_class: 'system-status-icon'});
+  update_icon(microphone.muted);
   button = new St.Bin({
     style_class: 'panel-button',
     reactive: true,
     can_focus: true,
     x_fill: true,
     y_fill: false,
-    track_hover: true});
-  icon = new St.Icon({
-    icon_name: 'microphone-sensitivity-high-symbolic',
-    style_class: 'system-status-icon'});
-  let stream = get_stream();
-  if (stream)
-    update_icon(stream.is_muted);
+    track_hover: true,
+    visible: microphone.active});
   button.set_child(icon);
   button.connect('button-press-event', on_activate);
-
-  microphone = new Microphone();
-  microphone.connect('activated', function() {
-    show_osd("Microphone activated", false);
+  microphone.connect('notify::active', function() {
+    button.visible = microphone.active;
+    if (initialised || microphone.active)
+      show_osd(
+        microphone.active ? "Microphone activated" : "Microphone deactivated",
+        microphone.muted);
+    initialised = true;
   });
-  microphone.connect('deactivated', function() {
-    show_osd("Microphone deactivated", true);
+  microphone.connect('notify::muted', function () {
+    update_icon(microphone.muted);
   });
 }
 
